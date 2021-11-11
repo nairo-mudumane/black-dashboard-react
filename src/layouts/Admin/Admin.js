2c88ab75c3d9bc4575d0c6154af0f730aaee92de
nairoMudumane <nazirmudumane@gmail.com>
2021-11-11 10:04:10 +0200

    no changes

--- src/layouts/Admin/Admin.js
+++ src/layouts/Admin/Admin.js
@@ -1,35 +1,18 @@
-/*!
-
-=========================================================
-* Black Dashboard React v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from "react";
-import { Route, Switch, Redirect, useLocation } from "react-router-dom";
+import React from 'react';
+import { Route, Switch, Redirect, useLocation } from 'react-router-dom';
 // javascript plugin used to create scrollbars on windows
-import PerfectScrollbar from "perfect-scrollbar";
+import PerfectScrollbar from 'perfect-scrollbar';
 
 // core components
-import AdminNavbar from "components/Navbars/AdminNavbar.js";
-import Footer from "components/Footer/Footer.js";
-import Sidebar from "components/Sidebar/Sidebar.js";
-import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
+import AdminNavbar from 'components/Navbars/AdminNavbar.js';
+import Footer from 'components/Footer/Footer.js';
+import Sidebar from 'components/Sidebar/Sidebar.js';
+import FixedPlugin from 'components/FixedPlugin/FixedPlugin.js';
 
-import routes from "routes.js";
+import routes from 'routes.js';
 
-import logo from "assets/img/react-logo.png";
-import { BackgroundColorContext } from "contexts/BackgroundColorContext";
+import logo from 'assets/img/react-logo.png';
+import { BackgroundColorContext } from 'contexts/BackgroundColorContext';
 
 var ps;
 
@@ -37,32 +20,32 @@ function Admin(props) {
   const location = useLocation();
   const mainPanelRef = React.useRef(null);
   const [sidebarOpened, setsidebarOpened] = React.useState(
-    document.documentElement.className.indexOf("nav-open") !== -1
+    document.documentElement.className.indexOf('nav-open') !== -1
   );
   React.useEffect(() => {
-    if (navigator.platform.indexOf("Win") > -1) {
-      document.documentElement.className += " perfect-scrollbar-on";
-      document.documentElement.classList.remove("perfect-scrollbar-off");
+    if (navigator.platform.indexOf('Win') > -1) {
+      document.documentElement.className += ' perfect-scrollbar-on';
+      document.documentElement.classList.remove('perfect-scrollbar-off');
       ps = new PerfectScrollbar(mainPanelRef.current, {
         suppressScrollX: true,
       });
-      let tables = document.querySelectorAll(".table-responsive");
+      let tables = document.querySelectorAll('.table-responsive');
       for (let i = 0; i < tables.length; i++) {
         ps = new PerfectScrollbar(tables[i]);
       }
     }
     // Specify how to clean up after this effect:
     return function cleanup() {
-      if (navigator.platform.indexOf("Win") > -1) {
+      if (navigator.platform.indexOf('Win') > -1) {
         ps.destroy();
-        document.documentElement.classList.add("perfect-scrollbar-off");
-        document.documentElement.classList.remove("perfect-scrollbar-on");
+        document.documentElement.classList.add('perfect-scrollbar-off');
+        document.documentElement.classList.remove('perfect-scrollbar-on');
       }
     };
   });
   React.useEffect(() => {
-    if (navigator.platform.indexOf("Win") > -1) {
-      let tables = document.querySelectorAll(".table-responsive");
+    if (navigator.platform.indexOf('Win') > -1) {
+      let tables = document.querySelectorAll('.table-responsive');
       for (let i = 0; i < tables.length; i++) {
         ps = new PerfectScrollbar(tables[i]);
       }
@@ -75,12 +58,12 @@ function Admin(props) {
   }, [location]);
   // this function opens and closes the sidebar on small devices
   const toggleSidebar = () => {
-    document.documentElement.classList.toggle("nav-open");
+    document.documentElement.classList.toggle('nav-open');
     setsidebarOpened(!sidebarOpened);
   };
   const getRoutes = (routes) => {
     return routes.map((prop, key) => {
-      if (prop.layout === "/admin") {
+      if (prop.layout === '/admin') {
         return (
           <Route
             path={prop.layout + prop.path}
@@ -99,7 +82,7 @@ function Admin(props) {
         return routes[i].name;
       }
     }
-    return "Brand";
+    return 'Brand';
   };
   return (
     <BackgroundColorContext.Consumer>
@@ -109,8 +92,8 @@ function Admin(props) {
             <Sidebar
               routes={routes}
               logo={{
-                outterLink: "https://www.creative-tim.com/",
-                text: "Creative Tim",
+                outterLink: 'https://www.creative-tim.com/',
+                text: 'Creative Tim',
                 imgSrc: logo,
               }}
               toggleSidebar={toggleSidebar}
@@ -127,7 +110,7 @@ function Admin(props) {
               </Switch>
               {
                 // we don't want the Footer to be rendered on map page
-                location.pathname === "/admin/maps" ? null : <Footer fluid />
+                location.pathname === '/admin/maps' ? null : <Footer fluid />
               }
             </div>
           </div>
